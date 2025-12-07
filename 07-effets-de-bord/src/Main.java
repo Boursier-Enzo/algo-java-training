593727738ab422cf7be1700593f191ba1950f183
Enzo Boursier <enzoboursier@outlook.fr>
2025-12-07 20:20:46 +0100

    exercice_7

--- 07-effets-de-bord/src/Main.java
+++ 07-effets-de-bord/src/Main.java
@@ -1,32 +1,33 @@
 public class Main {
-    public static void main(String[] args) {
-        int nombre = 10;
-        int[] tableau = {1, 2, 3};
 
-        System.out.println("Avant : nombre = " + nombre);
-        System.out.println("Avant : tableau[0] = " + tableau[0]);
+  public static void main(String[] args) {
+    int nombre = 10;
+    int[] tableau = { 1, 2, 3 };
 
-        modifierNombre(nombre);
-        modifierTableau(tableau);
+    System.out.println("Avant : nombre = " + nombre);
+    System.out.println("Avant : tableau[0] = " + tableau[0]);
 
-        System.out.println("Apres : nombre = " + nombre);
-        System.out.println("Apres : tableau[0] = " + tableau[0]);
+    modifierNombre(nombre);
+    modifierTableau(tableau);
 
-        nombre = doubler(nombre);
-        System.out.println("Apres doubler : nombre = " + nombre);
-    }
+    System.out.println("Apres : nombre = " + nombre);
+    System.out.println("Apres : tableau[0] = " + tableau[0]);
 
-    public static void modifierNombre(int n) {
-        n = n + 100;
-        System.out.println("Dans modifierNombre : n = " + n);
-    }
+    nombre = doubler(nombre);
+    System.out.println("Apres doubler : nombre = " + nombre);
+  }
 
-    public static void modifierTableau(int[] tab) {
-        tab[0] = tab[0] + 100;
-        System.out.println("Dans modifierTableau : tab[0] = " + tab[0]);
-    }
+  public static void modifierNombre(int n) {
+    n = n + 100;
+    System.out.println("Dans modifierNombre : n = " + n);
+  }
 
-    public static int doubler(int n) {
-        return n * 2;
-    }
+  public static void modifierTableau(int[] tab) {
+    tab[0] = tab[0] + 100;
+    System.out.println("Dans modifierTableau : tab[0] = " + tab[0]);
+  }
+
+  public static int doubler(int n) {
+    return n * 2;
+  }
 }
