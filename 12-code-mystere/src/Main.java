082ee846999880e81e5edf51efa075435af1f175
Enzo Boursier <enzoboursier@outlook.fr>
2025-12-09 18:25:00 +0100

    exercice_12

--- 12-code-mystere/src/Main.java
+++ 12-code-mystere/src/Main.java
@@ -1,24 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test 1 : mystere(12, 5)");
-        int r1 = mystere(12, 5);
-        System.out.println("Resultat : " + r1);
 
-        System.out.println("\nTest 2 : mystere(48, 18)");
-        int r2 = mystere(48, 18);
-        System.out.println("Resultat : " + r2);
+  public static void main(String[] args) {
+    System.out.println("Test 1 : mystere(12, 5)");
+    int r1 = mystere(12, 5);
+    System.out.println("Resultat : " + r1);
+
+    System.out.println("\nTest 2 : mystere(48, 18)");
+    int r2 = mystere(48, 18);
+    System.out.println("Resultat : " + r2);
+  }
+
+  public static int mystere(int a, int b) {
+    System.out.println("  mystere(" + a + ", " + b + ")");
+
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+      System.out.println("  -> a=" + a + ", b=" + b);
     }
 
-    public static int mystere(int a, int b) {
-        System.out.println("  mystere(" + a + ", " + b + ")");
-
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-            System.out.println("  -> a=" + a + ", b=" + b);
-        }
-
-        return a;
-    }
+    return a;
+  }
 }
